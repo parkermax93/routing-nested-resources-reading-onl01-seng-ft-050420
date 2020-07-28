2f408aac8879bdffc41d9acf242ac071185f0077
Parker Williamson <parker.williamson@gmail.com>
2020-07-28 16:35:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,15 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
-  
-  get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
-
+  Rails.application.routes.draw do
+   
+    resources :authors, only: [:show] do
+      # nested resource for posts
+      resources :posts, only: [:show, :index]
+    end
+   
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+   
+    root 'posts#index'
+  end
 end
